cee41b483809b6571457fa93a3952fdb3f3e114b
Ryan R <rasoarahonarivoniaina.h@ku.th>
2023-08-15 16:27:24 +0700

    correct

--- scripts/106_bedtools_multicov.sh
+++ scripts/106_bedtools_multicov.sh
@@ -11,5 +11,5 @@ for feature in "${features[@]}"
 do
   bedtools multicov --bams /tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/final_bam/*.bam \
     -bed "../data/106_annotation_ema/features/${feature}.bed" \
-    > "../data/106_annotation_ema/covearge/${feature}.txt"
+    > "../data/106_annotation_ema/coverage/${feature}.txt"
 done
