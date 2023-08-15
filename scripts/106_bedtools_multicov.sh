5fcdce36f12bf0d95bce829d9da6287678b679ce
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-08-15 16:28:57 +0700

    add 106

--- scripts/106_bedtools_multicov.sh
+++ scripts/106_bedtools_multicov.sh
@@ -9,7 +9,10 @@ features=(
 
 for feature in "${features[@]}"
 do
-  bedtools multicov --bams /tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/final_bam/*.bam \
+  bedtools multicov --bams \
+      /tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/final_bam/EMAf10_q20.bam \
+      /tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/final_bam/EMAf1_q20.bam \
+      /tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/final_bam/EMAf28_q20.bam \
     -bed "../data/106_annotation_ema/features/${feature}.bed" \
-    > "../data/106_annotation_ema/covearge/${feature}.txt"
+    > "../data/106_annotation_ema/coverage/${feature}.txt"
 done
